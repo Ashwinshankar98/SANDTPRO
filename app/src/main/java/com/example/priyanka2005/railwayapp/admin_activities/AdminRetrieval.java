a537dc8816ad9a94878da6baf7a373963067b794
Ashwinshankar98 <ashwinshankar5@gmail.com>
2018-07-03 22:46:39 +0530

    final version

--- app/src/main/java/com/example/priyanka2005/railwayapp/admin_activities/AdminRetrieval.java
+++ app/src/main/java/com/example/priyanka2005/railwayapp/admin_activities/AdminRetrieval.java
@@ -11,6 +11,8 @@ import com.example.priyanka2005.railwayapp.R;
 
 public class AdminRetrieval extends AppCompatActivity {
 
+    private boolean isAlreadyLoggedIn = false;
+
     private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,35 @@ public class AdminRetrieval extends AppCompatActivity {
         webSettings.setBuiltInZoomControls( true );
         myWebView.getSettings().setDomStorageEnabled(true);
         myWebView.loadUrl("https://southernrailwayapp.firebaseapp.com/");
-        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                try {
+                    if (url.contains("https://southernrailwayapp.firebaseapp.com/index.html") && isAlreadyLoggedIn) {
+                        // User is already logged in so show exit dialog and exit if user press yes
+                        //<Your Dialog Code>
+                        //finish();
+                        finish();
+                        //call finish(); when user press yes
+                    }
+                    else if (url.contains("https://southernrailwayapp.firebaseapp.com/Homepage/index.html")){
+                        /* if this is true then user is logged in because user will be redirected to
+                        DashBoard link only if he entered valid credentials.
+
+                        Now change our flag to true so next time when user is redirected to login screen
+                        then the first condition will get true and the exit dialog will be shown.
+                         */
+
+                        isAlreadyLoggedIn=true;
+                    }
+
+                    view.loadUrl(url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
         myWebView.setWebChromeClient( new WebChromeClient() );
     }
 
